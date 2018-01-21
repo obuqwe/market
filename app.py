cb294e5c0516807802e462c744ec08957491a878
Ilya Obukhov <mesisleo10@gmail.com>
2018-01-21 19:43:24 +0300

    add info about User-Agent

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello world"
+    return "<h1>Hello world</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+    print(request.headers)
+    return '<h1>Hello {}!</h1>'.format(name)
+
+@app.route('/agent')
+def user_agent():
+    #print(request.headers)
+    useragent = request.headers['User-Agent']
+    print(useragent)
+    return '<h3>Ваш юзерагент: {}</h3>'.format(useragent)
 
 if __name__ == "__main__":
     app.run(debug=True)
