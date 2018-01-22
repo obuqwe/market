6006addbfd7fd1d601010badef756b6d9f804ebd
Ilya Obukhov <mesisleo10@gmail.com>
2018-01-22 21:47:32 +0300

    add home and user page

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello world</h1>"
+    return redirect('https://google.ru')
 
 @app.route('/user/<name>')
 def user(name):
-    print(request.headers)
-    return '<h1>Hello {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 @app.route('/agent')
 def user_agent():
@@ -16,6 +15,27 @@ def user_agent():
     useragent = request.headers['User-Agent']
     print(useragent)
     return '<h3>Ваш юзерагент: {}</h3>'.format(useragent)
+
+@app.route('/home')
+def home_link():
+    info = {
+        'title': 'Welcome',
+        'description': 'Добро пожаловать',
+        'news': [
+            {'title': 'Breaking news'},
+            {'title': 'Hot news'},
+            {'title': 'New news!'},
+        ]
+    }
+    return render_template('home.html', info=info)
+
+@app.route('/404')
+def not_found():
+    return '<h1>Bad request</h1>', 404
+
+#@app.route('/')
+#def redirect_to_google():
+#    return redirect('https://google.ru')
 
 if __name__ == "__main__":
     app.run(debug=True)
